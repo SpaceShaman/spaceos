d1a30052406a43ec69340787434005e944417887
SpaceShaman <spaceshaman@tuta.io>
2026-09-20 14:14:00 +0000

    ♻️ (build scripts): remove obsolete build and run scripts to clean up the repository and reduce maintenance overhead

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo podman build \
-  --network=host \
-  --tag localhost/spaceos:latest \
-  --cap-add=all \
-  --security-opt=label=type:container_runtime_t \
-  --device /dev/fuse \
-  .
